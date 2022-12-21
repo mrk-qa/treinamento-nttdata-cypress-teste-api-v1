7e194350fc1cdb7f33afa4bfca1c3b751d13547e
mrk-qa <marco.ant.seg@gmail.com>
2022-12-21 19:47:09 -0300

    day 2 - test CRUD e refatoration

--- cypress/e2e/APItypeform.cy.js
+++ cypress/e2e/APItypeform.cy.js
@@ -1,20 +1,89 @@
 describe('API - Typeform', () => {
-  
+
+  const API_URL = 'https://api.typeform.com'
+  const AUTHORIZATION = ''   // insira seu token dentro de ''
+
   it('[GET] - Consulta dos dados do perfil', () => {
     cy.api({
       method: 'GET',
-      url: 'https://api.typeform.com/me',
+      url: `${API_URL}/me`,
       headers: {
-        authorization: 'Bearer tfp_7aPCzKcFWpG9MoqzZBi7xwwgGGwYkLRKU23SPxbrps3m_3pcooCXx3Pja8A'
+        authorization: `Bearer ${AUTHORIZATION}`
       }
-
     }).should((response) => {
       expect(response.status).to.eq(200)
       expect(response.body.alias).to.eq('Marco Antonio')
       expect(response.body.email).to.eq('marco.antoniosilva@nttdata.com')
       expect(response.body.language).to.eq('en')
-      expect(response.body.user_id).to.eq('01GJE15B7YPR923VQTGRK53A95')
-      expect(response.body.tracking_id).to.eq(22411884)
+    })
+  })
+
+  it('[GET] - Consulta dos dados do perfil (SEM TOKEN)', () => {
+    cy.api({
+      method: 'GET',
+      url: `${API_URL}/me`,
+      failOnStatusCode: false
+    }).should((response) => {
+      expect(response.status).to.eq(401)
+    })
+  })
+
+  it('[POST] - Criando um novo form', () => {
+    cy.api({
+      method: 'POST',
+      url: `${API_URL}/forms`,
+      headers: {
+        authorization: `Bearer ${AUTHORIZATION}`
+      },
+      body: {
+        title: 'NTTDATA - Treinamento de Testes Automatizados de API com Cypress'
+      }
+    }).should((response) => {
+      expect(response.status).to.eq(201)
+      Cypress.env('id_form', response.body.id)
+    })
+  })
+
+  it('[PATCH] - Substituindo um título do form', () => {
+    cy.api({
+      method: 'PATCH',
+      url: `${API_URL}/forms/` + Cypress.env('id_form'),
+      headers: {
+        authorization: `Bearer ${AUTHORIZATION}`
+      },
+      body: [
+        {
+          "op": "replace",
+          "path": "/title",
+          "value": "Novo título trocado pelo Cypress"
+        }
+      ]
+    }).should((response) => {
+      expect(response.status).to.eq(204)
+    })
+  })
+
+  it('[GET] - Busca por um form específico', () => {
+    cy.api({
+      method: 'GET',
+      url: `${API_URL}/forms/` + Cypress.env('id_form'),
+      headers: {
+        authorization: `Bearer ${AUTHORIZATION}`
+      },
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it('[DELETE] - Deletando um form que foi criado', () => {
+    cy.api({
+      method: 'DELETE',
+      url: `${API_URL}/forms/` + Cypress.env('id_form'),
+      headers: {
+        authorization: `Bearer ${AUTHORIZATION}`
+      },
+    }).should((response) => {
+      expect(response.status).to.eq(204)
     })
   })
 
